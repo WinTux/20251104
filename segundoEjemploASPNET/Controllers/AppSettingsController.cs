52f962642c8245e3a6f186aaa15fcd459f9107b2
rusok <rusok.software@gmail.com>
2025-11-06 19:54:03 -0400

    Parámetros

---
+++ segundoEjemploASPNET/Controllers/AppSettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace segundoEjemploASPNET.Controllers
+{
+    public class AppSettingsController : Controller
+    {
+        private IConfiguration configuration;
+
+        public AppSettingsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.attr1 = configuration["Atributo1"];
+            ViewBag.attr2 = configuration["Atributo2"];
+            ViewBag.config = configuration["MisConfiguraciones:Configuracion2"];
+            ViewBag.log = configuration["Logging:LogLevel:Microsoft.AspNetCore"];
+            return View();
+        }
+    }
+}
---
+++ segundoEjemploASPNET/Controllers/ParametrosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace segundoEjemploASPNET.Controllers
+{
+    [Route("Parametros")]
+    public class ParametrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Ejemplo2/{nom}")]
+        public IActionResult Ejemplo2(string nom){
+            ViewBag.nombre = nom;
+            return View("Example2");
+        }
+        [Route("Ejemplo2/{id1}/{id2}")]
+        public IActionResult Ejemplo3(int id1, int id2)
+        {
+            ViewBag.id1 = id1;
+            ViewBag.id2 = id2;
+            return View("Example3");
+        }
+    }
+}
