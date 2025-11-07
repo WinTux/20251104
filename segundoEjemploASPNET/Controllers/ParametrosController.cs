68a06c515056c230c9d1aa62559b344d60ae7f2d
rusok <rusok.software@gmail.com>
2025-11-07 19:31:08 -0400

    QueryStrings

--- segundoEjemploASPNET/Controllers/ParametrosController.cs
+++ segundoEjemploASPNET/Controllers/ParametrosController.cs
@@ -14,12 +14,21 @@ namespace segundoEjemploASPNET.Controllers
             ViewBag.nombre = nom;
             return View("Example2");
         }
-        [Route("Ejemplo2/{id1}/{id2}")]
+        [Route("Ejemplo3/{id1}/{id2}")]
         public IActionResult Ejemplo3(int id1, int id2)
         {
             ViewBag.id1 = id1;
             ViewBag.id2 = id2;
             return View("Example3");
+        }
+
+        [Route("Ejemplo4/{param1}-{param2}/usuario/{param3}")]
+        public IActionResult Ejemplo4(string param1, string param2, string param3)
+        {
+            ViewBag.id1 = param1;
+            ViewBag.id2 = param2;
+            ViewBag.id3 = param3;
+            return View("Example4");
         }
     }
 }
---
+++ segundoEjemploASPNET/Controllers/QueryStringsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace segundoEjemploASPNET.Controllers
+{
+    // Agregando route que rescata el nombre del controlador
+    [Route("{controller}")]
+    public class QueryStringsController : Controller
+    {
+        [Route("")] // Ruta vacía para la acción Index
+        [Route("Index")] // Ruta explícita para la acción Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("ejemplo1")] // Ruta para la acción ejemplo1
+        public IActionResult ejemplo1([FromQuery(Name = "id")] string parametro)
+        {
+            ViewBag.param = parametro;
+            return View("resultado");
+        }
+        [Route("ejemplo2")] // Ruta para la acción ejemplo2
+        public IActionResult ejemplo2([FromQuery(Name = "num1")] int n1, [FromQuery(Name = "num2")] int n2, [FromQuery(Name = "num3")] int n3)
+        {
+            ViewBag.numero1 = n1;
+            ViewBag.numero2 = n2;
+            ViewBag.numero3 = n3;
+            return View("resultado");
+        }
+    }
+}
