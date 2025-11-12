96dca963062ceda556e7d45fb650eb7f57ecd4dc
rusok <rusok.software@gmail.com>
2025-11-12 19:59:51 -0400

    Formularios con archivos 2

--- segundoEjemploASPNET/Controllers/ProductoController.cs
+++ segundoEjemploASPNET/Controllers/ProductoController.cs
@@ -5,8 +5,10 @@ namespace segundoEjemploASPNET.Controllers
     [Route("productos")] // https://localhost:7135/productos
     public class ProductoController : Controller
     {
-        public ProductoController() { 
-            // Continuar desde acá...
+        private IWebHostEnvironment webHostEnvironment;
+
+        public ProductoController(IWebHostEnvironment webHostEnvironment) { 
+            this.webHostEnvironment = webHostEnvironment;
         }
    
         [Route("")] // https://localhost:7135/productos
@@ -65,15 +67,47 @@ namespace segundoEjemploASPNET.Controllers
         {
             return View("Formulario1", new Models.Producto());
         }
+        [Route("formulario2")]
+        public IActionResult Formulario2()
+        {
+            return View("Formulario2", new para.formularios.Producto());
+        }
         [HttpPost]
         [Route("Registrar")] // https://localhost:7135/productos/registrar
         public IActionResult Registrar(Models.Producto producto, IFormFile foto) {
             if (foto == null || foto.Length == 0)
                 return Content("ARCHIVO NO SELECCIONADO O INVÁLIDO");
-            else { 
+            else {
+                var ruta = Path.Combine(webHostEnvironment.WebRootPath, "imagenes", foto.FileName);
+                using (var stream = new FileStream(ruta, FileMode.Create)) {
+                    foto.CopyTo(stream);
+                }
+                producto.Foto = foto.FileName;
+            }
+            ViewBag.producto = producto;
+            return View("Registrado");
+        }
+        [HttpPost]
+        [Route("Registrar2")] // https://localhost:7135/productos/registrar
+        public IActionResult Registrar2(para.formularios.Producto producto, IFormFile[] fotos)
+        {
+            if (fotos == null || fotos.Length == 0)
+                return Content("ARCHIVO NO SELECCIONADO O INVÁLIDO");
+            else
+            {
+                producto.Fotos = new List<string>();
+                foreach (IFormFile foto in fotos) {
+                    var ruta = Path.Combine(webHostEnvironment.WebRootPath, "imagenes", foto.FileName);
+                    using (var stream = new FileStream(ruta, FileMode.Create))
+                    {
+                        foto.CopyTo(stream);
+                    }
+                    producto.Fotos.Add(foto.FileName);
+                }
                 
             }
-            return View();
+            ViewBag.producto = producto;
+            return View("Registrado2");
         }
     }
 }
--- segundoEjemploASPNET/Models/Producto.cs
+++ segundoEjemploASPNET/Models/Producto.cs
@@ -9,3 +9,13 @@
         public int Cantidad { get; set; }
     }
 }
+namespace para.formularios {
+    public class Producto
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public List<string> Fotos { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
