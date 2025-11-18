60d60b32fc44893a6aedad5ea5060e478833efb4
rusok <rusok.software@gmail.com>
2025-11-18 18:57:06 -0400

    Sesiones 2

--- segundoEjemploASPNET/Controllers/LoginController.cs
+++ segundoEjemploASPNET/Controllers/LoginController.cs
@@ -8,5 +8,28 @@ namespace segundoEjemploASPNET.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Autenticacion(string username, string password)
+        {
+            // Aquí iría la lógica de autenticación real
+            if (username != null && password != null && username.Equals("pepe") && password.Equals("123456"))
+            {
+                // Autenticación exitosa
+                HttpContext.Session.SetString("Username", username);
+                return View("Exito");
+            }
+            else
+            {
+                // Autenticación fallida
+                ViewBag.ErrorMessage = "Credenciales inválidas. Inténtalo de nuevo.";
+                return View("Index");
+            }
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("Username");
+            return RedirectToAction("Index");
+        }
     }
 }
