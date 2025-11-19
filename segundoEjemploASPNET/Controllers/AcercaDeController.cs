b3b88efb4726f8010d753874f125c27fd1336382
rusok <rusok.software@gmail.com>
2025-11-19 19:51:46 -0400

    Layouts anidados

---
+++ segundoEjemploASPNET/Controllers/AcercaDeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace segundoEjemploASPNET.Controllers
+{
+    public class AcercaDeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Acerca1()
+        {
+            return View();
+        }
+        public IActionResult Acerca2()
+        {
+            return View();
+        }
+    }
+}
---
+++ segundoEjemploASPNET/Controllers/ContactoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace segundoEjemploASPNET.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto1()
+        {
+            return View();
+        }
+        public IActionResult Contacto2()
+        {
+            return View();
+        }
+        public IActionResult Contacto3()
+        {
+            return View();
+        }
+    }
+}
